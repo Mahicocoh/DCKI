ab8d319f59d7a9253826e01917eeed3fe7406ebd
We-Fixit <we-fixit@local>
2026-05-25 23:25:49 +0200

    Contact mobile: separations points forts; cache bump

--- admin/admin.js
+++ admin/admin.js
@@ -1,6 +1,6 @@
-import { showToast, formatRooms, mountToTopFab } from "../scripts/ui.js?v=202605252747";
+import { showToast, formatRooms, mountToTopFab } from "../scripts/ui.js?v=202605252840";
 import { getListingPhotos } from "../scripts/listings-data.js";
-import { getLang, initI18n, t } from "../scripts/i18n.js?v=202605252747";
+import { getLang, initI18n, t } from "../scripts/i18n.js?v=202605252840";
 
 const listHost = document.querySelector("[data-admin-list]");
 const countEl = document.querySelector("[data-admin-count]");
--- admin/login.js
+++ admin/login.js
@@ -1,5 +1,5 @@
-import { showToast, mountToTopFab } from "../scripts/ui.js?v=202605252747";
-import { initI18n, t } from "../scripts/i18n.js?v=202605252747";
+import { showToast, mountToTopFab } from "../scripts/ui.js?v=202605252840";
+import { initI18n, t } from "../scripts/i18n.js?v=202605252840";
 
 const qs = new URLSearchParams(window.location.search);
 const next = qs.get("next") || "/admin/index.html";
