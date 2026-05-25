7e5917560b47a5d91ba557d4e591ca505e5dbc79
We-Fixit <we-fixit@local>
2026-05-25 23:48:07 +0200

    Contact: dégradé sur image hero mobile; bump cache

--- admin/admin.js
+++ admin/admin.js
@@ -1,6 +1,6 @@
-import { showToast, formatRooms, mountToTopFab } from "../scripts/ui.js?v=202605260001";
+import { showToast, formatRooms, mountToTopFab } from "../scripts/ui.js?v=202605260015";
 import { getListingPhotos } from "../scripts/listings-data.js";
-import { getLang, initI18n, t } from "../scripts/i18n.js?v=202605260001";
+import { getLang, initI18n, t } from "../scripts/i18n.js?v=202605260015";
 
 const listHost = document.querySelector("[data-admin-list]");
 const countEl = document.querySelector("[data-admin-count]");
--- admin/login.js
+++ admin/login.js
@@ -1,5 +1,5 @@
-import { showToast, mountToTopFab } from "../scripts/ui.js?v=202605260001";
-import { initI18n, t } from "../scripts/i18n.js?v=202605260001";
+import { showToast, mountToTopFab } from "../scripts/ui.js?v=202605260015";
+import { initI18n, t } from "../scripts/i18n.js?v=202605260015";
 
 const qs = new URLSearchParams(window.location.search);
 const next = qs.get("next") || "/admin/index.html";
